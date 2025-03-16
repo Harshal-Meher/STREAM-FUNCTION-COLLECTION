4abf0822ad0e88dee3b2870f129178b119d37362
Harshal-Meher <harshalmeher294@gmail.com>
2025-03-16 17:57:32 +0530

    perticular Dept , emp indivisual dept

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -9,7 +6,7 @@ public class Main {
 
         List<Employee> list = Arrays.asList(
                 new Employee(101,"Harshal",23,50000,new Department("IT")),
-                new Employee(102,"Roshan",24,60000,new Department("Finance")),
+                new Employee(102,"Roshan",24,60000,new Department("IT")),
                 new Employee(103,"Ram",55,40000,new Department("HR")),
                 new Employee(104,"Jagrut",23,30000,new Department("ME"))
 
@@ -18,8 +15,31 @@ public class Main {
         Employee oldestEmployee = list.stream().max(Comparator.comparingInt(Employee::getEmpAge)).get();
         System.out.println("Oldest employee: " + oldestEmployee);
 
+        //Total number of employee indivisual department
+        Map<String, Long> totalNoEmp = list.stream()
+                .collect(Collectors.groupingBy(e -> e.getDept().getDeptName(), Collectors.counting()));
 
+        System.out.println("Total number of employees in each department:");
+        totalNoEmp.forEach((department, count) ->
+                System.out.println("Department: " + department + " | Total Employees: " + count)
+        );
+
+        // 3.1) List older employees for a particular department (like IT)
+        String departmentToFilter = "IT";
+        System.out.println("Older employees in department: " + departmentToFilter);
+
+//       list.stream().filter(e -> e.getDept().getDeptName().equalsIgnoreCase(departmentToFilter))
+//                .sorted(Comparator.comparingInt(Employee::getEmpAge).reversed())
+//                .forEach(e -> {
+//                    System.out.println(e);
+//                });
+
+       List<Employee> res = list.stream().filter(e -> e.getDept().getDeptName().equalsIgnoreCase(departmentToFilter))
+                .sorted(Comparator.comparingInt(Employee::getEmpAge).reversed()).collect(Collectors.toList());
+       res.forEach(employee -> {
+           System.out.println(employee);
+       });
+
+       }
     }
 
-
-}
